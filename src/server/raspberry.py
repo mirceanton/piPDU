5c1e432d2e7dda9ede503071c5d94f222ed87360
Mircea-Pavel ANTON <contact@mirceanton.com>
2022-12-04 21:50:28 +0200

    Add API endpoint to toggle sockets on/off

--- src/server/raspberry.py
+++ src/server/raspberry.py
@@ -1,8 +1,12 @@
 from config import Config
+from arduino import Arduino
 from flask import Flask
 
 # Parse the config file
 config = Config().data
+
+# Initialize serial connection to the arduino
+arduino = Arduino()
 
 # Create a Flask application
 app = Flask(__name__)
@@ -11,6 +15,25 @@ app = Flask(__name__)
 @app.route('/ping')
 def ping():
     return 'pong'
+
+# Flask route for sending commands to Arduino
+@app.route('/api/v1/sockets/<socket_id>', methods=['POST'])
+def send_command(number):
+    # Convert the socket number to an integer
+    number = int(number)
+
+    # Check if the number is in the valid range
+    if 0 <= number <= 15:
+        # Convert the number to a lowercase character
+        command = chr(number + 97)
+
+        # Write the command to the serial connection
+        arduino.write(command.encode())
+
+        print('Sent command to serial:', command)
+    else:
+        # Raise an exception with a custom error message if the number is out of range
+        raise Exception('Error: Socket number should be between 0 and 15')
 
 # Start the Flask API server
 app.run(host=config['api']['host'], port=config['api']['port'])
