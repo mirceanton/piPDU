82bd21a6f92002824b2e509c55bc4c25246b84cf
Mircea ANTON <contact@mirceanton.com>
2022-12-11 22:42:05 +0200

    [WIP] Initialize button array and configure interrupts

--- controller/src/controller.py
+++ controller/src/controller.py
@@ -6,6 +6,24 @@ import requests
 from common.lcd import LCD
 from common.config import Config
 
+def button_interrupt(pin):
+	# Scan the rows to detect a button press
+	for col_num, col_pin in enumerate(Config().btnArray.colPins):
+
+		# Set the row outputs to HIGH
+		for row_num, row_pin in enumerate(Config().btnArray.rowPins):
+			GPIO.output(row_pin, GPIO.HIGH)
+
+			# Check if the column input is high
+			if GPIO.input(col_pin) == GPIO.HIGH:
+				# Print the button number
+				print(col_num * len(Config().btnArray.rowPins) + row_num)
+				return
+
+		# Set the row outputs to LOW
+		for row_pin in Config().btnArray.rowPins:
+			GPIO.output(row_pin, GPIO.LOW)
+
 # Define piPDU server prometheus metrics endpoint
 METRICS_URL = f"http://{Config().api.host}:{Config().api.port}/metrics"
 
@@ -22,6 +40,15 @@ display.idle()
 for led in Config().led.pins:
 	GPIO.setup(led, GPIO.OUT)
 	GPIO.output(led, GPIO.LOW)
+
+# Initialize Button Array
+for row_pin in Config().btnArray.rowPins:
+	GPIO.setup(row_pin, GPIO.OUT)
+	GPIO.output(row_pin, GPIO.LOW)
+for col_pin in Config().btnArray.colPins:
+	GPIO.remove_event_detect(col_pin)
+	GPIO.setup(col_pin, GPIO.IN)
+	GPIO.add_event_detect(col_pin, GPIO.RISING, callback=button_interrupt, bouncetime=50)
 
 try:
 	while True:
