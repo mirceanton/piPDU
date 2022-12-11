71235708557ff61f1a49aa7f610cd11372fe1e75
Mircea ANTON <contact@mirceanton.com>
2022-12-11 20:28:44 +0200

    clean up main code

--- controller/src/controller.py
+++ controller/src/controller.py
@@ -1,35 +1,10 @@
 # Import the necessary libraries
 import RPi.GPIO as GPIO
-from RPLCD.i2c import CharLCD
+import time
+import requests
+from common.lcd import LCD
+from common.config import Config
 
-# Define connections
-led_pins = []     # TODO
-btn_arr_row = []  # TODO
-btn_arr_col = []  # TODO
-lcd = CharLCD(i2c_expander='PCF8574', address=0x27)
-
-# ===============================================
-# SETUP
-# ===============================================
-# Set up the GPIO
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-
-# Initialize the LEDs
-for pin in led_pins:
-  GPIO.setup(pin, GPIO.OUT)
-
-# Initialize button array
-for pin in btn_arr_row:
-  GPIO.setup(pin,GPIO.OUT)
-  GPIO.output(pin, GPIO.HIGH)
-
-for pin in btn_arr_col:
-  GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-# ===============================================
-# MAIN LOOP
-# ===============================================
-while True:
-  # TODO
-  pass
+display = LCD()
+display.idle()
+time.sleep(2)
