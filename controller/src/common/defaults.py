a5ab49e4bb817a1a62470a13371df20712707fed
Mircea ANTON <contact@mirceanton.com>
2022-12-11 21:56:00 +0200

    Change pins from BOARD to BCM notation

--- controller/src/common/defaults.py
+++ controller/src/common/defaults.py
@@ -1,9 +1,9 @@
 # Default config section
 metrics = { 'enabled': True }
-led = {'pins':  [33, 35, 37, 40, 38, 36, 32, 30, 28, 26, 24, 22, 18, 16, 12, 10, 8]}
+led = {'pins':  [13, 19, 26, 21, 20, 16, 12, 1, 7, 8, 25, 24, 23, 18, 15, 14]}
 btnArray = {
-	'rowPins': [19, 23],
-	'colPins': [21, 23, 29, 31, 15, 13, 11, 7],
+	'rowPins': [10, 11],
+	'colPins': [9, 0, 5, 6, 22, 27, 27, 17, 4],
 	'longPressDurationSeconds': 5,
 }
 lcd = {
