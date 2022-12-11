bed1cf7630518a9efa51bcefa5d5673de32d3b47
Mircea ANTON <contact@mirceanton.com>
2022-12-11 21:58:07 +0200

    Initialize LEDs

--- controller/src/controller.py
+++ controller/src/controller.py
@@ -5,6 +5,19 @@ import requests
 from common.lcd import LCD
 from common.config import Config
 
+# Initialize GPIO
+GPIO.setwarnings(False)		# Disable GPIO Warnings
+GPIO.cleanup()				# Clean up the GPIO pins just in case they're in a bad state
+GPIO.setmode(GPIO.BCM)	# Set up the GPIO pins for output
+
+# Initialize LCD Display
 display = LCD()
 display.idle()
-time.sleep(2)
+
+# Initialize LEDs
+for led in Config().led.pins:
+	GPIO.setup(led, GPIO.OUT)
+	GPIO.output(led, GPIO.LOW)
+
+# Clean up the GPIO pins
+GPIO.cleanup()
