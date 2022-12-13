578d8c1aa6e25fa3c7d85ca2414b5783cee67dcb
Mircea ANTON <contact@mirceanton.com>
2022-12-13 17:55:56 +0200

    Update defaults

--- controller/src/common/defaults.py
+++ controller/src/common/defaults.py
@@ -1,13 +1,13 @@
 # Default config section
 metrics = {
 	'enabled': True,
-	'periodSeconds': 1,
+	'pollPeriodSeconds': 1,
 }
 led = {'pins':  [13, 19, 26, 21, 20, 16, 12, 1, 7, 8, 25, 24, 23, 18, 15, 14]}
 btnArray = {
 	'rowPins': [10, 11],
-	'colPins': [9, 0, 5, 6, 22, 27, 27, 17, 4],
-	'longPressDurationSeconds': 5,
+	'colPins': [9, 0, 5, 6, 22, 27, 17, 4],
+	'longPressDurationSeconds': 3,
 }
 lcd = {
 	'expander': "PCF8574",
