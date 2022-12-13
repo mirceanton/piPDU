1cd7d527784f0b55b77e5b68342483a69cee991b
Mircea ANTON <contact@mirceanton.com>
2022-12-13 17:57:04 +0200

    Refactor using new classes

--- controller/src/controller.py
+++ controller/src/controller.py
@@ -1,29 +1,39 @@
-from common.functions import setup, loop
+# Import the necessary libraries
+import RPi.GPIO as GPIO
+import time
+from common.lcd import LCD
+from common.led_array import led_array_init, led_array_update
+from common.button_matrix import ButtonMatrix
+from common.config import Config
+from common.metrics import Metrics
 
-setup()
+# =================================================================================================
+# SETUP
+# =================================================================================================
+GPIO.setwarnings(False)	# Disable GPIO Warnings
+GPIO.setmode(GPIO.BCM)	# Set up the GPIO pins for output
+GPIO.cleanup()			# Clean up the GPIO pins just in case they're in a bad state
+ButtonMatrix()			# Inititalize the buttons
+led_array_init()		# Initialize LEDs
+LCD().idle()			# Initialize LCD Display
 
+# =================================================================================================
+# METRICS THREAD
+# =================================================================================================
+def metrics_loop():
+	Metrics().collect()	# Collect the metrics
+	led_array_update()	# Update the LED status indicators
+	LCD.update()
+	time.sleep(Config().metrics.pollPeriodSeconds)
+
+metrics_thread = threading.Thread(target=metrics_loop)
+metrics_thread.start()
+
+# =================================================================================================
+# MAIN LOOP
+# =================================================================================================
 try:
 	while True:
-		loop()
-
-# Clean up the GPIO pins when the script is interrupted
+		ButtonMatrix().poll()
 except:
 	GPIO.cleanup()
-
-def button_interrupt(pin):
-	# Scan the rows to detect a button press
-	for col_num, col_pin in enumerate(Config().btnArray.colPins):
-
-		# Set the row outputs to HIGH
-		for row_num, row_pin in enumerate(Config().btnArray.rowPins):
-			GPIO.output(row_pin, GPIO.HIGH)
-
-			# Check if the column input is high
-			if GPIO.input(col_pin) == GPIO.HIGH:
-				# Print the button number
-				print(col_num * len(Config().btnArray.rowPins) + row_num)
-				return
-
-		# Set the row outputs to LOW
-		for row_pin in Config().btnArray.rowPins:
-			GPIO.output(row_pin, GPIO.LOW)
