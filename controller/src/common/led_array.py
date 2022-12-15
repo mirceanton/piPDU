684c475939bcc9c4f186e273703c767a3a43da53
Mircea ANTON <contact@mirceanton.com>
2022-12-15 23:01:25 +0200

    Implement led_array_update

--- controller/src/common/led_array.py
+++ controller/src/common/led_array.py
@@ -1,14 +1,14 @@
 import RPi.GPIO as GPIO
 from common.config import Config
-
+from common.metrics import Metrics
 
 # Update LED status based on relay status
 def led_array_update():
-	pass
+    for index, led in enumerate(Config().led.pins):
+        GPIO.output(led, GPIO.HIGH if Metrics().metrics[index] > 0 else GPIO.LOW) 
 
 # Initialize the LEDs by confoiguring the pins as outputs and setting them to LOW
 def led_array_init():
-	for led in Config().led.pins:
-		GPIO.setup(led, GPIO.OUT)
-		GPIO.output(led, GPIO.HIGH)
-
+    for led in Config().led.pins:
+        GPIO.setup(led, GPIO.OUT)
+        GPIO.output(led, GPIO.HIGH)
