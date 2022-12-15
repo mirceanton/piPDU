311c58c0b908db947815d4f1ed8c40e5e2ffedd7
Mircea ANTON <contact@mirceanton.com>
2022-12-15 23:02:06 +0200

    Change indentation to spaces. Implement short and long press

--- controller/src/common/button_matrix.py
+++ controller/src/common/button_matrix.py
@@ -1,57 +1,71 @@
 import RPi.GPIO as GPIO
 from common.config import Config
 import datetime
+import requests
+from common.lcd import LCD, LCDStates
 
 class ButtonMatrix:
-	# Singleton instance of the ButtonMatrix class
-	__instance = None
+    # Singleton instance of the ButtonMatrix class
+    __instance = None
 
     # Store the current and previous states of each button in the matrix
-	__old_states = [False] * 16
-	__cur_state = [False] * 16
+    __old_states = [False] * 16
+    __cur_state = [False] * 16
 
     # Store the time at which each button was pressed
-	__times = [0.0] * 16
+    __times = [0.0] * 16
 
-	def poll(self):
-		for index, state in enumerate(self.__cur_state):
-			self.__old_states[index] = state
+    def poll(self):
+        # Update old state = state
+        for index, state in enumerate(self.__cur_state):
+            self.__old_states[index] = state
 
-		for col in Config().btnArray.colPins:
-			GPIO.output(col, GPIO.HIGH)
+        for col in Config().btnArray.colPins:
+            GPIO.output(col, GPIO.HIGH)
 
-			for row in Config().btnArray.rowPins:
-				ri = Config().btnArray.rowPins.index(row)
-				ci = Config().btnArray.colPins.index(col)
-				index = ri * 8 + ci
+            for row in Config().btnArray.rowPins:
+                # Calculate the button index
+                row_index = Config().btnArray.rowPins.index(row)
+                col_index = Config().btnArray.colPins.index(col)
+                btn_index = row_index * 8 + col_index
 
-				# Update the current state of the buttons
-				self.__cur_state[index] = ( GPIO.input(row) == GPIO.HIGH )
+                # Update the current state of the button
+                self.__cur_state[btn_index] = ( GPIO.input(row) == GPIO.HIGH )
 
-				# Check if the button has been pressed
-				if self.__cur_state[index] is True and self.__old_states[index] is False:
-					self.__times[index] = datetime.datetime.now()
-						
-				# Check if the button has been released
-				if self.__cur_state[index] is False and self.__old_states[index] is True:
-						time_held = (datetime.datetime.now() - self.__times[index]).total_seconds()
-						if time_held >  Config().btnArray.longPressDurationSeconds:
-							print(f"Button {index} long press")
-						else:
-							print(f"Button {index} short press")
+                # If the button has just been pressed, record the time
+                if self.__cur_state[btn_index] is True and self.__old_states[btn_index] is False:
+                    self.__times[btn_index] = datetime.datetime.now()
 
-			GPIO.output(col, GPIO.LOW)
+                # If the button has been released, see for how long it has been pressed
+                if self.__cur_state[btn_index] is False and self.__old_states[btn_index] is True:
+                    time_held = (datetime.datetime.now() - self.__times[btn_index]).total_seconds()
+                    if time_held >  Config().btnArray.longPressDurationSeconds:
+                        self.__long_press(btn_index)
+                    else:
+                        self.__short_press(btn_index)
 
-	# Initialize the pins used for the button matrix
-	def __init__(self):
-		for pin in Config().btnArray.colPins:
-			GPIO.setup(pin, GPIO.OUT)
+            GPIO.output(col, GPIO.LOW)
 
-		for pin in Config().btnArray.rowPins:
-			GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    # Handle a long press on one of the buttons -> toggle relay
+    def __long_press(self, index):
+        resp = requests.post(f"http://{Config().api.host}:{Config().api.port}/api/v1/sockets/{index}")
+        print(resp)
+        LCD().setState(LCDStates.IDLE) # idle
 
-	# Method for creating a singleton instance of the ButtonMatrix class
-	def __new__(cls):
-		if cls.__instance is None:
-			cls.__instance = object.__new__(cls)
-		return cls.__instance
+    # Handle a short press on one of the buttons -> set LCD to info
+    def __short_press(self, index):
+        LCD().setState(index)
+
+    # Initialize the pins used for the button matrix
+    def __init__(self):
+        for pin in Config().btnArray.colPins:
+            GPIO.setup(pin, GPIO.OUT)
+
+        for pin in Config().btnArray.rowPins:
+            GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+    # Method for creating a singleton instance of the ButtonMatrix class
+    def __new__(cls):
+        if cls.__instance is None:
+            cls.__instance = object.__new__(cls)
+        return cls.__instance
