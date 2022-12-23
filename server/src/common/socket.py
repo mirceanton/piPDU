f88a95c38e60afcedf2d7ee65ea6553948988da6
Mircea Anton <contact@mirceanton.com>
2022-12-23 20:51:37 +0200

    Parse the socket config file && Add socket config sample

--- server/src/common/socket.py
+++ server/src/common/socket.py
@@ -17,10 +17,14 @@ class SocketArray:
 
     with open('sockets.yaml') as file:
       data = yaml.load(file, Loader=yaml.FullLoader)
-      print(data)
   
-    for i in range(0, 16):
-      self.sockets.append(Socket(i))
+      for i in range(0, 16):
+        sck = data[f"socket_{i}"] if f"socket_{i}" in data else {}
+        self.sockets.append(Socket(
+          id = i,
+          name = sck['name'] if 'name' in sck else None,
+          initState = sck['initState'] == "True" if 'initState' in sck else True
+        ))
 
 class Socket:
   id = None
@@ -28,7 +32,7 @@ class Socket:
   initState = None
   state = None
 
-  def __init__(self, id, name=None, initState=True):
+  def __init__(self, id=0, name=None, initState=True):
     self.id = id
     self.name = name if name is not None else f"socket_{id}"
     self.initState = initState
@@ -48,7 +52,7 @@ class Socket:
     return 1
 
   def turnOff(self, force=False):
-    if not self.state:
+    if self.state is False:
       return 0
 
     Arduino().write(chr(self.id + 97)) # 97 is the ASCII code for "a"
