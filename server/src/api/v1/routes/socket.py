00e29851a5d2fe08a63b6ab5d79309865914f2e9
Mircea Anton <contact@mirceanton.com>
2022-12-25 00:02:23 +0200

    Add socket toggle endpoint

--- server/src/api/v1/routes/socket.py
+++ server/src/api/v1/routes/socket.py
@@ -14,14 +14,18 @@ blueprint = Blueprint('socket', __name__)
 
 @blueprint.before_request
 def index_validator():
-    """Check if the `number` argument from each request is within the valid [0,15] range."""
+    """
+    Check if the `number` argument from each request is within the valid [0,15] range.
+    """
     number = request.view_args.get('number')
     if number < 0 or number > 15:
         return resp.index_out_of_range()
 
 @blueprint.route('/<int:number>/info', methods=['GET'])
 def status(number: int):
-    """Check the status of a given socket"""
+    """
+    Check the status of a given socket
+    """
     return make_response(jsonify({
         'success': True,
         'payload': SocketArray().sockets[number].info()
@@ -29,14 +33,26 @@ def status(number: int):
 
 @blueprint.route('/<int:number>/on', methods=['POST'])
 def on(number: int):
-    """Turn on a given socket"""
+    """
+    Turn on a given socket
+    """
     if SocketArray().sockets[number].turnOn() == 0:
         return resp.nothing_to_do()
     return resp.socket_on_ok(number)
 
 @blueprint.route('/<int:number>/off', methods=['POST'])
 def off(number: int):
-    """Turn off a given socket"""
+    """
+    Turn off a given socket
+    """
     if SocketArray().sockets[number].turnOff() == 0:
         return resp.nothing_to_do()
+    return resp.socket_off_ok(number)
+
+@blueprint.route('/<int:number>/toggle', methods=['POST'])
+def off(number: int):
+    """
+    Toggle a given socket
+    """
+    SocketArray().sockets[number].toggle()
     return resp.socket_off_ok(number)
