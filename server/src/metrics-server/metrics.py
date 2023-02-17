59bbce1ea28c679ee01daf31b6756095e2002278
Mircea ANTON <contact@mirceanton.com>
2023-02-17 11:38:21 +0200

    Refactor code.

--- server/src/metrics-server/metrics.py
+++ server/src/metrics-server/metrics.py
@@ -8,6 +8,7 @@ RABBITMQ_PORT = os.environ['RABBITMQ_PORT']
 RABBITMQ_PATH = os.environ['RABBITMQ_PATH']
 RABBITMQ_USER = os.environ['RABBITMQ_USER']
 RABBITMQ_PASS = os.environ['RABBITMQ_PASS']
+RABBITMQ_QUEUE = "metrics"
 
 # RabbitMQ connection parameters
 credentials = pika.PlainCredentials(RABBITMQ_USER, RABBITMQ_PASS)
@@ -16,38 +17,40 @@ parameters = pika.ConnectionParameters(RABBITMQ_HOST, RABBITMQ_PORT, RABBITMQ_PA
 # Create a RabbitMQ connection and channel, and declare the queue
 connection = pika.BlockingConnection(parameters)
 channel = connection.channel()
-channel.queue_declare(queue='metrics')
+channel.queue_declare(queue=RABBITMQ_QUEUE)
 
-# Create the list for the prometheus gauges and start the metrics server
+# Create the list for the prometheus gauges 
 gauges = []
 for i in range(16):
-    gauge = Gauge(f'metric_{i}', f'Metric {i}', ['unit'])
+    gauge = Gauge(f'socket_{i}', f'Socket {i}', ['Amps'])
     gauges.append(gauge)
 
+# Start the metrics server
 start_http_server(8000)
+print('INFO: Prometheus Metrics Server started on port 8000')
 
 # Define the callback function for handling incoming messages
 def queue_message_callback(ch, method, properties, body):
     # Parse the message into a list of float values
     message = body.decode('utf-8').rstrip()
+    print(f'INFO: Got message from queue: {message}')
     values = map(float, message.split(' '))
-    
+
     # Update the gauges with the new values
     for i, value in enumerate(values):
         print(i, ":", value)
-        gauges[i].labels(unit='A').set(value)
-  
+        gauges[i].labels(unit='Amps').set(value)
+
 # Start consuming messages from the RabbitMQ queue
 channel.basic_consume(
-  queue='metrics',
-  on_message_callback=queue_message_callback,
-  auto_ack=True
+    queue=RABBITMQ_QUEUE,
+    on_message_callback=queue_message_callback,
+    auto_ack=True
 )
 
-print('Metrics server is listening for messages on the metrics queue...')
-
 try:
+    print('INFO: Metrics server is listening for messages on the metrics queue...')
     channel.start_consuming()
 except KeyboardInterrupt:
+    # Gracefully close the connection with the RabbitMQ server
     connection.close()
-
