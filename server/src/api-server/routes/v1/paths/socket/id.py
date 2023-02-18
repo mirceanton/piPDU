cc590cc4ce070f2bba2a495dface2d1fdb816f48
Mircea ANTON <contact@mirceanton.com>
2023-02-18 20:19:14 +0200

    Implement paths using helpers

--- server/src/api-server/routes/v1/paths/socket/id.py
+++ server/src/api-server/routes/v1/paths/socket/id.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, request, make_response
+from flask import Blueprint, request, make_response, jsonify
+import routes.v1.utils.socket as socket
 
 blueprint = Blueprint('socket', __name__)
 
@@ -6,22 +7,50 @@ blueprint = Blueprint('socket', __name__)
 def index_validator():
     number = request.view_args.get('number')
     if number < 0 or number > 15:
-        print("index out of range")
-        return "index out of range"
+        print('ERROR: Index out of range')
+        return make_response(jsonify({
+            'status': False,
+            'payload': {
+                'error': 'Index out of range',
+                'message': 'Socket id must be between 0 and 15'
+            }
+        }), 400)
 
 @blueprint.route('/<int:number>/info', methods=['GET'])
-def status(number: int):
-    return make_response(jsonify({
-        'success': True,
-        'payload': "info here"
-    }), 200)
+def info(number: int):
+    print(f'DEBUG: Requested info for socket {number}')
+    return make_response(jsonify(
+        socket.sockets[number].info()
+    ))
 
 @blueprint.route('/<int:number>/on', methods=['POST'])
 def on(number: int):
-    print(f'Turn on socket {number}')
-    return "ok"
+    print(f'DEBUG: Requested ON state for socket {number}')
+    return make_response(jsonify(
+        socket.sockets[number].on()
+    ))
 
 @blueprint.route('/<int:number>/off', methods=['POST'])
 def off(number: int):
-    print(f'Turn off socket {number}')
-    return "ok"
+    print(f'DEBUG: Requested OFF state for socket {number}')
+    return make_response(jsonify(
+        socket.sockets[number].off()
+    ))
+
+@blueprint.route('/<int:number>/on', methods=['PUT'])
+def setStateOn(number: int):
+    print(f'DEBUG: Requested state update for socket {number} to ON')
+    socket.sockets[number].state = True
+    return make_response(jsonify({
+        'status': True,
+        'payload': {}
+    }), 200)
+
+@blueprint.route('/<int:number>/off', methods=['PUT'])
+def setStateOff(number: int):
+    print(f'DEBUG: Requested state update for socket {number} to OFF')
+    socket.sockets[number].state = False
+    return make_response(jsonify({
+        'status': True,
+        'payload': {}
+    }), 200)
