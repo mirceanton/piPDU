d8c6ec9e08e4d23554363c5994ce175123be3707
Mircea ANTON <contact@mirceanton.com>
2023-02-21 21:11:22 +0200

    Make all vars configurable and set defaults for some

--- server/src/metrics-server/utils/constants.py
+++ server/src/metrics-server/utils/constants.py
@@ -1,10 +1,10 @@
 import os
 
 # Get RabbitMQ credentials from environment variables
-RABBITMQ_HOST = os.environ['RABBITMQ_HOST']
-RABBITMQ_PORT = os.environ['RABBITMQ_PORT']
-RABBITMQ_PATH = os.environ['RABBITMQ_PATH']
-RABBITMQ_USER = os.environ['RABBITMQ_USER']
-RABBITMQ_PASS = os.environ['RABBITMQ_PASS']
-RABBITMQ_METRICS_QUEUE = 'metrics'
-RABBITMQ_CONSUMER_TAG = 'metrics_server'
+RABBITMQ_HOST = os.environ.get('RABBITMQ_HOST', 'rabbitmq')
+RABBITMQ_PORT = os.environ.get('RABBITMQ_PORT', '5672')
+RABBITMQ_PATH = os.environ.get('RABBITMQ_PATH', '/')
+RABBITMQ_USER = os.environ.get('RABBITMQ_USER')
+RABBITMQ_PASS = os.environ.get('RABBITMQ_PASS')
+RABBITMQ_METRICS_QUEUE = os.environ.get('RABBITMQ_METRICS_QUEUE', 'metrics')
+RABBITMQ_CONSUMER_TAG = os.environ.get('RABBITMQ_CONSUMER_TAG', 'metrics_server')
