c075b751f13139a0373aae41fc752db8deaf3c15
Mircea Anton <contact@mirceanton.com>
2023-02-24 20:06:20 +0000

    [Server] Initial Implementation.

---
+++ server/src/api-server/utils/rabbitmq.py
@@ -0,0 +1,39 @@
+import pika
+
+class RabbitMQ:
+    def __init__(self, username, password, host, port, path):
+        self.connection = None
+        self.channel = None
+
+        # RabbitMQ connection parameters
+        credentials = pika.PlainCredentials(username, password)
+        parameters = pika.ConnectionParameters(host, port, path, credentials)
+
+        # Create a RabbitMQ connection and channel, and declare the queue
+        try:
+            self.connection = pika.BlockingConnection(parameters)
+            print('INFO: Connection established to RabbitMQ')
+            self.channel = self.connection.channel()
+        except pika.exceptions.AMQPConnectionError:
+            print('ERROR: Could not connect to RabbitMQ. Check your connection settings.')
+            exit(1)
+
+    def declareQueue(self, queue: str):
+        self.channel.queue_declare(queue = queue)
+
+    def publish(self, queue: str, message: str):
+        try:
+            self.channel.basic_publish(
+                exchange = '',
+                routing_key = queue,
+                body = message
+            )
+            print("INFO: Sent message to queue: " + message)
+            return True, None
+        except Exception as ex:
+            print(f'ERROR: An error occured publishing the message: {ex}')
+            return False, ex
+
+    def close(self):
+        print(f'INFO: Closing the RabbitMQ Connection.')
+        self.connection.close()
---
+++ server/src/arduino/arduino.ino
@@ -0,0 +1,74 @@
+#include "Relay.h"
+#include "ACS712.h"
+
+const int numSockets = 16;
+unsigned long time;
+
+// ================================================================================================
+// RELAY VARIABLES
+// ================================================================================================
+const int relayPins[numSockets] = {41, 42, 44, 49, 50, 52, 38, 43, 46, 51, 39, 40, 45, 47, 48, 53};
+Relay *relays[numSockets];
+
+// ================================================================================================
+// SENSOR VARIABLES
+// ================================================================================================
+const int sensorPins[numSockets] = {A11, A10, A1, A7, A6, A2, A14, A15, A5, A0, A9, A12, A13, A3, A4, A1};
+ACS712 *sensors[numSockets];
+
+void setup() {
+  Serial.begin(921600);
+
+  for (int i = 0; i < numSockets; i++) {
+    relays[i] = new Relay(relayPins[i], true);
+    sensors[i] = new ACS712(sensorPins[i], 145);
+    sensors[i]->setNoiseFloor(0.1);
+  }
+}
+
+void loop() {
+  for(int i = 0; i < 500; i++) {
+    for (int j = 0; j < numSockets; j++) {
+      sensors[j]->poll();
+    }
+  }
+
+  String message = "";
+  for (int i = 0; i < numSockets; i++) {
+    message += String(sensors[i]->getAmps()) + " ";
+  }
+  Serial.println(message);
+  Serial.flush();
+}
+
+void serialEvent() {
+  if (Serial.available() <= 0) return;
+
+  const char message = Serial.read();
+
+  if (message == 'q') {
+    for (int i = 0; i < numSockets; i++) {
+      relays[i]->off();
+    }
+    return;
+  }
+
+  if (message == 'r') {
+    for (int i = 0; i < numSockets; i++) {
+      relays[i]->on();
+    }
+    return;
+  }
+
+  if (isUpperCase(message)) {
+    const int relayId = (message - 'A');
+    if (0 <= relayId && relayId < 16) {
+      relays[relayId]->on();
+    }
+  } else {
+    const int relayId = (message - 'a');
+    if (0 <= relayId && relayId < 16) {
+      relays[relayId]->off();
+    }
+  }
+}
