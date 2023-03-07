e39a9dfd0a7716647b92172efb6e24e21e7fdc14
Mircea Anton <contact@mirceanton.com>
2023-03-07 21:22:23 +0000

    [Controller] Implement Functionality

---
+++ controller/src/display-controller/src/display-controller.py
@@ -0,0 +1,55 @@
+import utils.constants as constants
+from utils.metrics import scrape_metrics
+from utils.display import Display, DisplayState
+import time
+import json
+import os
+
+print('DEBUG: Initializing LCD display')
+display = Display(
+    expander=constants.DISPLAY_I2C_EXPANDER,
+    i2c_bus=constants.DISPLAY_I2C_BUS,
+    i2c_address=constants.DISPLAY_I2C_ADDR,
+    backlight=constants.DISPLAY_BACKLIGHT_ENABLED
+)
+
+print(f'DEBUG: Creating named pipe {constants.FIFO}')
+if not os.path.exists(constants.FIFO):
+    os.mkfifo(constants.FIFO)
+
+print('DEBUG: Opening named pipe in read-only mode')
+pipe = open(constants.FIFO, 'r')
+
+
+def pipe_message_callback(body):
+    if len(body) == 0:
+        return
+
+    print(f'DEBUG: Got message from pipe: {body}')
+    data = json.loads(body)
+    print(f'DEBUG: Parsed JSON as: {data}')
+
+    state = DisplayState[data['state'].upper()]
+    socket = int(data['socket']) if 'socket' in data else None
+
+    if state == DisplayState.IDLE and socket is not None:
+        raise ValueError(
+            f'Poorly formatted message; No socket should be set for IDLE state: {data}')
+
+    if state == DisplayState.INFO and not (0 <= socket < 16):
+        raise ValueError(
+            f'Poorly formatted message; Invalid socket number for INFO state: {data}')
+
+    display.set_state(state, socket)
+
+
+print('INFO: Starting main loop')
+try:
+    while True:
+        pipe_message_callback(pipe.read())
+        display.update(scrape_metrics(constants.METRICS_ENDPOINT_URL))
+        time.sleep(constants.METRICS_POLL_INTERVAL_SECONDS)
+except KeyboardInterrupt:
+    print('INFO: Received Keyboard Interrupt')
+    display.close()
+    pipe.close()
---
+++ controller/src/led-controller/src/utils/constants.py
@@ -0,0 +1,38 @@
+from pcf8574 import PCF8574
+import os
+
+EXPANDER_I2C_BUS = [int(x) for x in os.environ.get('EXPANDER_I2C_BUS').split(",")]
+EXPANDER_I2C_ADDR = [int(x, 16) for x in os.environ.get('EXPANDER_I2C_ADDR').split(",")]
+
+LED_PINS = [int(pin) for pin in os.environ.get('BUTTON_PINS', '1,3,5,7,9,11,13,15,0,2,4,6,8,10,12').split(",")]
+LED_UPDATE_INTERVAL_SECONDS = int(os.environ.get('LED_UPDATE_INTERVAL_SECONDS', '1'))
+PIPDU_SERVER_ADDRESS = os.environ.get('PIPDU_SERVER_ADDRESS')
+
+
+def get_expanders():
+    if len(EXPANDER_I2C_BUS) < 1:
+        raise ValueError("""
+            At least 1 i2c bus must be speciffied!
+            EXPANDER_I2C_BUS is empty
+        """.strip())
+
+    # If there are multiple busses specified, there should be 1 bus per address
+    if len(EXPANDER_I2C_BUS) > 1 and len(EXPANDER_I2C_BUS) != len(EXPANDER_I2C_ADDR):
+        raise ValueError("""
+          If more than 1 i2c bus is specified, there should be an i2c bus per i2c address.
+          EXPANDER_I2C_BUS and EXPANDER_I2C_ADDR must have the same number of elements.
+        """.strip())
+
+    expanders = []
+
+    # If there is only 1 bus specified, the same bus will be used for all expanders
+    if len(EXPANDER_I2C_BUS) == 1:
+        bus = EXPANDER_I2C_BUS[0]
+        for addr in EXPANDER_I2C_ADDR:
+            expanders.append(PCF8574(bus, addr))
+        return expanders
+
+    # Otherwise, use the specified i2c bus for the expander
+    for bus, addr in zip(EXPANDER_I2C_BUS, EXPANDER_I2C_ADDR):
+        expanders.append(PCF8574(bus, addr))
+    return expanders
