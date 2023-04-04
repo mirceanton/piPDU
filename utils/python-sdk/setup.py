bb2d39cad1886cf74af82fa9b0f82090a12b05fe
Mircea-Pavel ANTON <contact@mirceanton.com>
2023-04-04 19:26:09 +0000

    fix

--- utils/python-sdk/setup.py
+++ utils/python-sdk/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='pipdu',
-    packages=['src'],
+    packages=['pipdu'],
     version='0.0.0',
     license='MIT',
     description='PiPDU python SDK',
