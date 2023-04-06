c4a3220e828c69f9d692d5853d1e1461ada746d4
Mircea-Pavel ANTON <contact@mirceanton.com>
2023-04-06 19:05:34 +0000

    Update key comparison

--- utils/python-sdk/pipdu-sdk/config/v1_0_0.py
+++ utils/python-sdk/pipdu-sdk/config/v1_0_0.py
@@ -16,6 +16,6 @@ def validate_server_dict(server):
     if not isinstance(server, dict):
         raise ValueError('Invalid `servers`. Expected list of maps.')
 
-    unsupported_keys = set(server.keys()) - set(['id', 'name', 'host', 'apiPort', 'metricsPort'])
+    unsupported_keys = server.keys() - ['id', 'name', 'host', 'apiPort', 'metricsPort']
     if (len(unsupported_keys) > 0):
         raise ValueError(f"Unsupported key(s) in server: {unsupported_keys}")
