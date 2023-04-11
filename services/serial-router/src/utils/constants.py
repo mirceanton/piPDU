f964a9b29da3684c8080a06e7ddb7b5c42f4c4b5
Mircea-Pavel ANTON <contact@mirceanton.com>
2023-04-12 01:13:26 +0300

    Remove unused service

--- services/serial-router/src/utils/constants.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# Get the serial device and baud rate from environment variables
-SERIAL_DEVICE = os.environ.get('SERIAL_DEVICE', '/dev/ttyACM0')
-SERIAL_BAUDRATE = int(os.environ.get('SERIAL_BAUDRATE', '921600'))
-
-METRICS_FIFO = os.environ.get('METRICS_FIFO', '/tmp/metrics_fifo')
-COMMANDS_FIFO = os.environ.get('COMMANDS_FIFO', '/tmp/commands_fifo')
