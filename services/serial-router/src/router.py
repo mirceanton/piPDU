89fa934b3bac2a8e2579b9d02e1036a43cc31ea0
Mircea-Pavel ANTON <contact@mirceanton.com>
2023-04-11 16:31:37 +0000

    Migrate to named pipe

--- services/serial-router/src/router.py
+++ services/serial-router/src/router.py
@@ -3,26 +3,30 @@ from utils.arduino import Arduino
 import utils.constants as constants
 import json
 import time
+import os
 
 arduino = Arduino(
     device=constants.SERIAL_DEVICE,
     baud_rate=constants.SERIAL_BAUDRATE
 )
 
-rabbitmq = RabbitMQ(
-    username=constants.RABBITMQ_USER,
-    password=constants.RABBITMQ_PASS,
-    host=constants.RABBITMQ_HOST,
-    port=constants.RABBITMQ_PORT,
-    path=constants.RABBITMQ_PATH
-)
-rabbitmq.declareQueue(constants.RABBITMQ_COMMANDS_QUEUE)
-rabbitmq.declareQueue(constants.RABBITMQ_METRICS_QUEUE)
+print(f'DEBUG: Creating named pipe {constants.METRICS_FIFO}')
+if not os.path.exists(constants.METRICS_FIFO):
+    os.mkfifo(constants.METRICS_FIFO)
 
+print('DEBUG: Opening named pipe in write-only mode')
+metrics_pipe = open(constants.METRICS_FIFO, 'w')
 
-def queue_message_callback(body):
+print(f'DEBUG: Creating named pipe {constants.COMMANDS_FIFO}')
+if not os.path.exists(constants.COMMANDS_FIFO):
+    os.mkfifo(constants.COMMANDS_FIFO)
+
+print('DEBUG: Opening named pipe in read-only mode')
+commands_pipe = open(constants.COMMANDS_FIFO, 'r')
+
+def pipe_message_callback(body):
     data = json.loads(body.decode('utf-8'))
-    print(f'INFO: Got message from queue: {data}')
+    print(f'INFO: Got message from pipe: {data}')
 
     if data['id'] is None:
         return arduino.write('r' if data['state'] is True else 'q')
@@ -34,28 +38,18 @@ def queue_message_callback(body):
 
 try:
     while True:
-        if rabbitmq.hasMessage(queue=constants.RABBITMQ_COMMANDS_QUEUE):
-            rabbitmq.consume(
-                queue=constants.RABBITMQ_COMMANDS_QUEUE,
-                callback=queue_message_callback
-            )
-        else:
-            print("No Rabbit")
+        pipe_message_callback(commands_pipe.read())
 
         if arduino.hasMessage():
             message = arduino.read()
             print(f'DEBUG: Message read from arduino: {message}')
-            rabbitmq.publish(
-                queue=constants.RABBITMQ_METRICS_QUEUE,
-                message=message
-            )
+            os.write(metrics_pipe, message);
             print('INFO: Message enqueued')
-        else:
-            print("No Arduino")
 
         time.sleep(0.1)
 
 except KeyboardInterrupt:
     print('INFO: Received Keyboard Interrupt')
     arduino.close()
-    rabbitmq.close()
+    metrics_pipe.close()
+    commands_pipe.close()
--- services/serial-router/src/utils/constants.py
+++ services/serial-router/src/utils/constants.py
@@ -4,12 +4,5 @@ import os
 SERIAL_DEVICE = os.environ.get('SERIAL_DEVICE', '/dev/ttyACM0')
 SERIAL_BAUDRATE = int(os.environ.get('SERIAL_BAUDRATE', '921600'))
 
-# Get RabbitMQ credentials from environment variables
-RABBITMQ_HOST = os.environ.get('RABBITMQ_HOST', 'rabbitmq')
-RABBITMQ_PORT = os.environ.get('RABBITMQ_PORT', '5672')
-RABBITMQ_PATH = os.environ.get('RABBITMQ_PATH', '/')
-RABBITMQ_USER = os.environ.get('RABBITMQ_USER')
-RABBITMQ_PASS = os.environ.get('RABBITMQ_PASS')
-RABBITMQ_METRICS_QUEUE = os.environ.get('RABBITMQ_METRICS_QUEUE', 'metrics')
-RABBITMQ_COMMANDS_QUEUE = os.environ.get('RABBITMQ_COMMANDS_QUEUE', 'commands')
-RABBITMQ_CONSUMER_TAG = os.environ.get('RABBITMQ_CONSUMER_TAG', 'serial_router')
+METRICS_FIFO = os.environ.get('METRICS_FIFO', '/tmp/metrics_fifo')
+COMMANDS_FIFO = os.environ.get('COMMANDS_FIFO', '/tmp/commands_fifo')
