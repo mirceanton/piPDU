aa3b34f6b0fc007842f5cc691f37581e7ce2e35f
Mircea-Pavel ANTON <contact@mirceanton.com>
2023-04-11 16:31:25 +0000

    Make sure the pipe is created

--- services/api-server/src/api.py
+++ services/api-server/src/api.py
@@ -3,6 +3,11 @@ from routes.ping import blueprint as ping_blueprint
 from routes.sockets.all import blueprint as sockets_all_blueprint
 from routes.sockets.by_id import blueprint as sockets_by_id_blueprint
 from flask import Flask
+import os
+
+print(f'DEBUG: Creating named pipe {constants.FIFO}')
+if not os.path.exists(constants.FIFO):
+    os.mkfifo(constants.FIFO)
 
 # Create a Flask application and register the endpoints
 app = Flask(__name__)
